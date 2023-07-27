0817e275f5f48e66cd2ec0121ae8644e61c654bf
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-07-27 19:56:51 -0400

    added back button to book view

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -8,6 +8,11 @@
     <x-booksearch></x-booksearch>
 
     <div>
+        <a class="flex items-center my-3 text-lg font-semibold text-slate-300" href="{{ url()->previous() }}"><svg
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+            </svg>Back</a>
         <div class="text-lg text-center border shadow-md py-7 text-slate-300 border-slate-800 shadow-slate-600">
             <img class="w-1/2 mx-auto mb-4 border rounded-sm lg:w-1/6 border-slate-300"
                 src="{{ $book->volumeInfo->imageLinks->thumbnail ?? url('/images/book.jpg') }}" alt="">
