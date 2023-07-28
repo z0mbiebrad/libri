57ed2c427a04e20e10e2c6ea46b75e9c630ca5a0
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-07-27 20:44:48 -0400

    finished styling for singular book view

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -16,12 +16,32 @@
         <div class="text-lg text-center border shadow-md py-7 text-slate-300 border-slate-800 shadow-slate-600">
             <img class="w-1/2 mx-auto mb-4 border rounded-sm lg:w-1/6 border-slate-300"
                 src="{{ $book->volumeInfo->imageLinks->thumbnail ?? url('/images/book.jpg') }}" alt="">
-            <h5 class="p-1 underline">{{ $book->volumeInfo->title ?? null }}</h5>
-            <h5 class="p-1 text-base">{{ $book->volumeInfo->subtitle ?? null }}</h5>
-            <h5 class="p-1 text-base">{{ $book->volumeInfo->authors[0] ?? null }}</h5>
-            <p class="p-1 italic">{{ $book->volumeInfo->categories[0] ?? null }}</p>
-            <p class="p-1 italic">Published Date: {{ $book->volumeInfo->publishedDate ?? null }}</p>
-            <p class="pt-1 text-base">{!! $book->volumeInfo->description !!}</p>
+            <h5 class="p-1 py-2 underline border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                {{ $book->volumeInfo->title ?? null }}
+            </h5>
+            <h5 class="p-1 py-2 text-base border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                {{ $book->volumeInfo->subtitle ?? null }}
+            </h5>
+            <h5 class="p-1 py-2 text-base border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                {{ $book->volumeInfo->authors[0] ?? null }}
+            </h5>
+            <p class="p-1 py-2 italic border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                {{ $book->volumeInfo->categories[0] ?? null }}
+            </p>
+            <p
+                class="flex items-center justify-center p-1 py-2 italic border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                Rating:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                </svg>{{ $book->volumeInfo->averageRating ?? null }}
+            </p>
+            <p class="p-1 py-2 italic border rounded-md shadow-md border-slate-600 shadow-slate-600">Published Date:
+                {{ $book->volumeInfo->publishedDate ?? null }}
+            </p>
+            <p class="py-2 pt-1 text-base border rounded-md shadow-md border-slate-600 shadow-slate-600">
+                {!! $book->volumeInfo->description !!}</p>
         </div>
     </div>
     {{-- <div><a class="text-slate-300" href="/next">next</a></div> --}}
