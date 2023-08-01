bc1825332e4f9688aa324f9c920cd91157974704
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-07-31 21:32:38 -0400

    Update booksearch.blade.php
    made it required so users couldnt search for nothing

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -2,7 +2,7 @@
     <form action="{{ route('results') }}" class="text-lg font-semibold">
         <label for="bookSearch" class="block w-full py-2 text-center"></label>
         <input id="bookSearch" name="bookSearch" class="block w-3/4 mx-auto text-black rounded-lg"
-            placeholder="Name of book...">
+            placeholder="Name of book..." required>
         <button class="block w-full py-2">Search</button>
     </form>
 </div>
