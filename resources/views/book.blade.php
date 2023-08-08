3917db13beb28583ec130c105fb802745aad8b85
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-08-08 14:10:43 -0400

    Update book.blade.php
    made buttons for adding books to lists styled

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -25,14 +25,15 @@
             </svg>Back</a>
         <div class="text-lg text-center border shadow-md text-slate-300 border-slate-800 shadow-slate-600">
             @if (Auth::user())
-                <div class="flex justify-center">
+                <div class="flex justify-around">
                     <form class="p-2" action="{{ route('addFinished') }}" method="POST" class="">
                         @csrf
                         <input type="hidden" name="google_book_id" value="{{ $book->id }}"
                             class="flex items-center">
-                        <button type="submit" class="text-slate-300"><svg xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                class="inline-block w-6 h-6">
+                        <button type="submit"
+                            class="flex items-center p-2 mx-auto border rounded-md shadow-md text-slate-300 border-slate-600 shadow-slate-600"><svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="inline-block w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                             Finished</button>
@@ -40,12 +41,24 @@
                     <form class="p-2" action="{{ route('addCurrent') }}" method="POST">
                         @csrf
                         <input type="hidden" name="google_book_id" value="{{ $book->id }}">
-                        <button type="submit" class="text-slate-300"> Current</button>
+                        <button type="submit"
+                            class="flex items-center p-2 mx-auto border rounded-md shadow-md text-slate-300 border-slate-600 shadow-slate-600"><svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="inline-block w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            Current</button>
                     </form>
                     <form class="p-2" action="{{ route('addWishlist') }}" method="POST">
                         @csrf
                         <input type="hidden" name="google_book_id" value="{{ $book->id }}">
-                        <button type="submit" class="text-slate-300"> Wishlist</button>
+                        <button type="submit"
+                            class="flex items-center p-2 mx-auto border rounded-md shadow-md text-slate-300 border-slate-600 shadow-slate-600"><svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="inline-block w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                            </svg>
+                            Wishlist</button>
                     </form>
                 </div>
             @else
