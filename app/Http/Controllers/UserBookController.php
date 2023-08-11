aa3e73d4f0e8b1b9a0f9ca3ae436ca8eecd49ac1
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-08-11 18:11:44 -0400

    WIP
    moved store method from bookcontroller to userbook controller, and deleted unnecessary files

--- app/Http/Controllers/UserBookController.php
+++ app/Http/Controllers/UserBookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use App\Models\UserBook;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -38,9 +39,40 @@ class UserBookController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Book $book, Request $request)
     {
-        //
+        if ($request->finished_google_book_id) {
+            $list = 'finished';
+            $message = 'finished';
+        }
+        if ($request->current_google_book_id) {
+            $list = 'current';
+            $message = 'current';
+        }
+        if ($request->wishlist_google_book_id) {
+            $list = 'wishlist';
+            $message = 'wishlist';
+        }
+        if (UserBook::where('google_book_id', $book->google_book_id)->where('list', $list)->exists()) {
+            return view('book-search.results-book', ['book' => $book])->with('message', 'That book is already in your ' . $message . ' reading list.');
+        };
+        UserBook::create([
+            'user_id' => Auth::id(),
+            'book_id' => $book->id,
+            'list' => $list,
+            'google_book_id' => $book->google_book_id,
+            'thumbnail' => $book->thumbnail ?? null,
+            'title' => $book->title ?? null,
+            'subtitle' => $book->subtitle ?? null,
+            'authors' => $book->authors ?? null,
+            'categories' => $book->categories ?? null,
+            'rating' => $book->averageRating ?? null,
+            'published_date' => $book->published_date ?? null,
+            'description' => $book->description ?? null,
+            'publisher' => $book->publisher ?? null,
+        ]);
+
+        return view('book-search.results-book', ['book' => $book])->with('message', 'Book added to ' . $message . ' reading list.');
     }
 
     /**
@@ -72,23 +104,10 @@ class UserBookController extends Controller
      */
     public function destroy(UserBook $book)
     {
-        if ($book->list === 'finished') {
-            $book->delete();
+        $list = $book->list;
+        $book->delete();
 
-            return redirect()->route('finished.index')
-                ->with('message', 'Book deleted successfully.');
-        }
-        if ($book->list === 'current') {
-            $book->delete();
-
-            return redirect()->route('current.index')
-                ->with('message', 'Book deleted successfully.');
-        }
-        if ($book->list === 'wishlist') {
-            $book->delete();
-
-            return redirect()->route('wishlist.index')
-                ->with('message', 'Book deleted successfully.');
-        }
+        return redirect()->route('list.index', $list)
+            ->with('message', 'Book deleted from ' . $list . ' list successfully.');
     }
 }
