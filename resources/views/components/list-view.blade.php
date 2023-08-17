b2d82f40e54c633f1a5febe1074c8823256744bb
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-08-17 19:31:18 -0400

    Update list-view.blade.php

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -8,22 +8,22 @@
         src="{{ $book->thumbnail ?? url('/images/book.jpg') }}" alt="">
 </a>
 @isset($book->title)
-    <h5 class="p-1 py-2 text-base underline border rounded-md shadow-md border-slate-600 shadow-slate-600">
+    <h5 class="p-1 py-2 text-base underline border rounded-md border-slate-600 ">
         {{ $book->title }}</h5>
 @endisset
 @isset($book->subtitle)
-    <h5 class="p-1 py-2 text-base border rounded-md shadow-md border-slate-600 shadow-slate-600">
+    <h5 class="p-1 py-2 text-base border rounded-md border-slate-600 ">
         {{ $book->subtitle }}</h5>
 @endisset
 @isset($book->authors)
-    <h5 class="p-1 py-2 text-base border rounded-md shadow-md border-slate-600 shadow-slate-600">
+    <h5 class="p-1 py-2 text-base border rounded-md border-slate-600 ">
         {{ $book->authors }}</h5>
 @endisset
 @isset($book->categories)
-    <p class="p-1 py-2 text-base italic border rounded-md shadow-md border-slate-600 shadow-slate-600">
+    <p class="p-1 py-2 text-base italic border rounded-md border-slate-600 ">
         {{ $book->categories }}</p>
 @endisset
 @isset($book->published_date)
-    <p class="p-1 py-2 text-base italic border rounded-md shadow-md border-slate-600 shadow-slate-600">Published
+    <p class="p-1 py-2 text-base italic border rounded-md border-slate-600 ">Published
         Date: {{ $book->published_date }}</p>
 @endisset
