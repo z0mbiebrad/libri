d5c1569fd08756bdce3a656085612398aeb7e1bb
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-09-07 19:03:28 -0400

    Update list-view.blade.php
    made text orientation left to right and centered it positionally to the container its in

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -3,27 +3,26 @@
 @endphp
 
 <a name="book" href="{{ route($href, $book) }}">
-    <p class="pt-6 pb-6 underline">More information</p>
+    <p class="pt-6 pb-6 text-center underline">More information</p>
     <img class="w-1/2 mx-auto mb-4 border rounded-sm shadow-lg shadow-slate-700 lg:w-1/6 border-slate-700"
         src="{{ $book->thumbnail ?? url('/images/book.jpg') }}" alt="">
 </a>
 @isset($book->title)
-    <h5 class="p-1 py-2 mx-auto text-base underline rounded-md">
+    <h5 class="w-1/2 p-1 py-2 mx-auto text-base underline rounded-md">
         {{ $book->title }}</h5>
 @endisset
 @isset($book->subtitle)
-    <h5 class="p-1 py-2 text-base rounded-md">
+    <h5 class="w-1/2 p-1 py-2 mx-auto text-base rounded-md">
         {{ $book->subtitle }}</h5>
 @endisset
 @isset($book->authors)
-    <h5 class="p-1 py-2 text-base rounded-md">
+    <h5 class="w-1/2 p-1 py-2 mx-auto text-base rounded-md">
         {{ $book->authors }}</h5>
 @endisset
 @isset($book->categories)
-    <p class="p-1 py-2 text-base italic rounded-md">
+    <p class="w-1/2 p-1 py-2 mx-auto text-base italic rounded-md">
         {{ $book->categories }}</p>
 @endisset
 @isset($book->published_date)
-    <p class="p-1 py-2 mb-4 text-base italic rounded-md">Published
-        Date: {{ $book->published_date }}</p>
+    <p class="w-1/2 p-1 py-2 mx-auto mb-4 text-base italic rounded-md">Published: {{ $book->published_date }}</p>
 @endisset
