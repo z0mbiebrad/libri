0ab85bfb2c92e2332094d6bf9f081e8a47640ab8
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-09-20 20:44:50 -0400

    Fixed redirect, and refixed back button

--- resources/views/book-search/results-book.blade.php
+++ resources/views/book-search/results-book.blade.php
@@ -7,8 +7,10 @@
 
     <x-booksearch />
 
-    @if (!empty($message))
-        <div class="text-lg text-center text-slate-300"> {{ $message }}</div>
+    @if (session('status'))
+        <div class="ml-2 alert alert-success text-slate-300">
+            {{ session('status') }}
+        </div>
     @endif
 
     <div>
