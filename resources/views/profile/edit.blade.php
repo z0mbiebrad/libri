f6951bc7eaf5f22fb482dd718f4f24da063a0680
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-09-20 20:52:48 -0400

    Changed profile page to match color scheme

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,19 +13,19 @@
 
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
+            <div class="p-4 shadow bg-slate-900 sm:p-8 sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
+            <div class="p-4 shadow bg-slate-900 sm:p-8 sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white shadow sm:p-8 sm:rounded-lg">
+            <div class="p-4 shadow bg-slate-900 sm:p-8 sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
