1614bce8e03c84008db6c9c49e25e34215d43dc5
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-10-19 09:36:35 -0400

    Deleted unnecessary files and created a UserBook factor

--- database/factories/UserBookFactory.php
+++ database/factories/UserBookFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FinishedBook>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserBook>
  */
-class FinishedBookFactory extends Factory
+class UserBookFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(1)->create();
+        User::factory(1)->create();
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'poop'
