8f3d60ff0578bdd0b9d18c3305125a0380cf1e12
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-10-19 16:42:02 -0400

    Update app.blade.php
    changed background to black

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
+<body class="font-sans antialiased bg-black">
     <div class="min-h-screen bg-black">
         @include('layouts.navigation')
 
