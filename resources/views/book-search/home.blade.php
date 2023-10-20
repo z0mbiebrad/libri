4f872e745d88bf2ba683c8150f4ce222eb3f4557
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-10-19 21:08:11 -0400

    Update home.blade.php
    unnecessary div and styling

--- resources/views/book-search/home.blade.php
+++ resources/views/book-search/home.blade.php
@@ -7,27 +7,25 @@
 
     <x-booksearch />
 
-    <div class="flex items-center h-full">
-        <div class="p-3 m-auto text-xl sm:text-2xl text-slate-300 sm:w-4/5">
-            <h3 class="px-4 py-4">Welcome to Libri Search!</h3>
-            <p class="px-4 py-4">
-                Libri Search is a user-friendly book search tool. Discover, organize, and curate your reading journey
-                effortlessly with three distinct lists: Finished Books, Currently Reading, and Wishlist.
-            </p>
-            <p class="px-4 py-4">
-                Just type in the full or partial name of a book, and you will be presented with a list of the
-                top
-                ten closest matches. Found what you're looking for? A simple selection lets you add it seamlessly to
-                your
-                preferred list.
-            </p>
-            <p class="px-4 py-4">
-                While the search feature is accessible to all, utilizing the full potential of Libri Search's Book Lists
-                requires a quick account creation and login.
-            </p>
-            <p class="px-4 py-4">
-                Happy reading!
-            </p>
-        </div>
+    <div class="w-5/6 h-full p-3 m-auto text-xl sm:text-2xl text-slate-300">
+        <h3 class="py-4">Welcome to Libri Search!</h3>
+        <p class="py-4">
+            Libri Search is a user-friendly book search tool. Discover, organize, and curate your reading journey
+            effortlessly with three distinct lists: Finished Books, Currently Reading, and Wishlist.
+        </p>
+        <p class="py-4">
+            Just type in the full or partial name of a book, and you will be presented with a list of the
+            top
+            ten closest matches. Found what you're looking for? A simple selection lets you add it seamlessly to
+            your
+            preferred list.
+        </p>
+        <p class="py-4">
+            While the search feature is accessible to all, utilizing the full potential of Libri Search's Book Lists
+            requires a quick account creation and login.
+        </p>
+        <p class="py-4">
+            Happy reading!
+        </p>
     </div>
 </x-app-layout>
