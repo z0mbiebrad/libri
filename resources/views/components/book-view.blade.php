4cbd0f8a31b482e4df9c77416fcd3de05f76e34f
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-10-30 16:43:20 -0400

    Update book-view.blade.php
    changed order of information

--- resources/views/components/book-view.blade.php
+++ resources/views/components/book-view.blade.php
@@ -6,20 +6,25 @@
             {{ $book->title }}
         </h5>
     @endisset
+    @isset($book->published_date)
+        <p class="py-2 italic">
+            {{ $book->published_date }}
+        </p>
+    @endisset
     @isset($book->subtitle)
         <h5 class="py-2">
             {{ $book->subtitle }}
-        </h5>
-    @endisset
-    @isset($book->authors)
-        <h5 class="py-2">
-            {{ $book->authors }}
         </h5>
     @endisset
     @isset($book->categories)
         <p class="py-2 italic">
             {{ $book->categories }}
         </p>
+    @endisset
+    @isset($book->authors)
+        <h5 class="py-2">
+            {{ $book->authors }}
+        </h5>
     @endisset
     @isset($book->rating)
         <p class="flex items-center py-2 italic">
@@ -29,11 +34,6 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
             </svg>
-        </p>
-    @endisset
-    @isset($book->published_date)
-        <p class="py-2 italic">
-            {{ $book->published_date }}
         </p>
     @endisset
     <p class="py-5">
