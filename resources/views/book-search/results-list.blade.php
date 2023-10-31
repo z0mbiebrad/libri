0dfff7f6d32a203071c0ebb78935c4d1d734467e
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-10-31 07:41:57 -0400

    Update results-list.blade.php
    turned add to list into a dropdown aswell

--- resources/views/book-search/results-list.blade.php
+++ resources/views/book-search/results-list.blade.php
@@ -10,10 +10,22 @@
     @foreach ($books as $book)
         <div class="mx-auto text-lg border text-slate-300 border-slate-800 lg:w-1/2">
             @auth
-                <div class="flex justify-around pt-4">
-                    <x-list-add name="finished" :book="$book" />
-                    <x-list-add name="current" :book="$book" />
-                    <x-list-add name="wishlist" :book="$book" />
+                <div class="pt-4">
+                    <div x-data="{ show: false }" @click.away="show = false" class="w-5/6 m-3 mx-auto">
+                        <div @click="show = ! show" class="flex">
+                            <button>
+                                <p class="underline">Add to a list</p>
+                            </button>
+                        </div>
+                        <div x-show="show" style="display:none" class="flex">
+                            @if (Auth::user())
+                                <x-list-add name="finished" :book="$book" />
+                                <x-list-add name="current" :book="$book" />
+                                <x-list-add name="wishlist" :book="$book" />
+                            @endif
+                        </div>
+                    </div>
+
                 </div>
             @endauth
 
