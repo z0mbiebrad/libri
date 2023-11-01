7d2dcf11e2c5726e24580bec67718114d123c47d
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-01 06:59:35 -0400

    Update navigation.blade.php
    Made conditional so if user is logged in their name is displayed by the menu dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -87,7 +87,11 @@
             <div class="flex items-center -mr-2 sm:hidden">
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
-                    <p class="mr-2">Menu</p>
+                    @auth
+                        <p class="mr-2">{{ Auth::user()->name }}</p>
+                    @else
+                        <p class="mr-2">Menu</p>
+                    @endauth
                     <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
