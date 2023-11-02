fb9ece984996d06607a22f3e3e6efdedeeb2e4c9
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-02 08:20:41 -0400

    Style test

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -1,64 +1,68 @@
-<div x-data="{ show: false }">
-    <div class="flex items-center justify-around">
-        <img class="border rounded-sm shadow-lg shadow-slate-600 lg:w-1/6 border-slate-600"
-            src="{{ $book->thumbnail ?? url('/images/book.jpg') }}" alt="">
-        <div>
-            @isset($book->title)
-                <p class="py-2">
-                    {{ $book->title }}
-                </p>
-            @endisset
-            @isset($book->authors)
-                <p class="py-2">
-                    {{ $book->authors }}
-                </p>
-            @endisset
-        </div>
-    </div>
+<div>
+    {{-- <div class="flex items-center justify-around"> --}}
+    <div class="w-11/12 mx-auto text-center">
 
-    {{-- Trigger --}}
-    <div @click="show = ! show" class="flex justify-center mt-2">
-        <button class="flex items-center py-2 mb-4 border-b-2">
-            More information
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6 pt-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
-            </svg>
-        </button>
-    </div>
-
-    {{-- Links  --}}
-    <div x-show="show" style="display:none">
-        <div>
-            @isset($book->subtitle)
-                <p class="py-2 italic">
-                    {{ $book->subtitle }}
-                </p>
-            @endisset
-            @isset($book->categories)
-                <p class="py-2 italic">
-                    {{ $book->categories }}
-                </p>
-            @endisset
-            @isset($book->published_date)
-                <p class="py-2 italic">
-                    {{ $book->published_date }}
-                </p>
-            @endisset
-            @isset($book->rating)
-                <p class="flex items-center py-2 italic">
-                    {{ $book->rating }}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                    </svg>
-                </p>
-            @endisset
-            <p class="py-2 mb-5">
-                {!! strip_tags($book->description, '<b><br><i>') ?? 'Description not available.' !!}
+        @isset($book->title)
+            <p class="py-2 text-xl font-bold">
+                {{ $book->title }}
             </p>
+        @endisset
+        <img class="flex justify-center mx-auto my-2 border rounded-sm shadow-lg shadow-slate-600 lg:w-1/6 border-slate-600"
+            src="{{ $book->thumbnail ?? url('/images/book.jpg') }}" alt="">
+
+        @isset($book->authors)
+            <p class="py-2 text-xl italic">
+                {{ $book->authors }}
+            </p>
+        @endisset
+    </div>
+    {{-- </div> --}}
+    <div x-data="{ show: false }">
+
+        {{-- Trigger --}}
+        <div @click="show = ! show" class="flex justify-center">
+            <button class="flex items-center py-2 mb-4 border-b-2">
+                More information
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6 pt-2">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
+                </svg>
+            </button>
+        </div>
+
+        {{-- Links  --}}
+        <div x-show="show" style="display:none">
+            <div>
+                @isset($book->subtitle)
+                    <p class="py-2 italic">
+                        {{ $book->subtitle }}
+                    </p>
+                @endisset
+                @isset($book->categories)
+                    <p class="py-2 italic">
+                        {{ $book->categories }}
+                    </p>
+                @endisset
+                @isset($book->published_date)
+                    <p class="py-2 italic">
+                        {{ $book->published_date }}
+                    </p>
+                @endisset
+                @isset($book->rating)
+                    <p class="flex items-center py-2 italic">
+                        {{ $book->rating }}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="gold" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                        </svg>
+                    </p>
+                @endisset
+                <p class="py-2 mb-5">
+                    {!! strip_tags($book->description, '<b><br><i>') ?? 'Description not available.' !!}
+                </p>
+            </div>
         </div>
     </div>
 </div>
