c6c00f0d76a99ee1cc09444acb6aa6ea6eb960f9
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-08 13:47:20 -0500

    Color change

--- resources/views/book-search/results-list.blade.php
+++ resources/views/book-search/results-list.blade.php
@@ -10,7 +10,7 @@
     <div class="lg:grid lg:grid-cols-3 ">
         @foreach ($books as $book)
             <div
-                class="w-5/6 sm:max-w-md rounded-md mx-auto mb-6 text-lg border shadow-inner text-slate-300 shadow-slate-600 border-slate-600 lg:mt-6 {{ $loop->iteration === 1 ? 'mt-6' : '' }}">
+                class="w-5/6 sm:max-w-md rounded-md mx-auto mb-6 text-lg border shadow-inner bg-slate-900 text-slate-300 shadow-slate-600 border-slate-600 lg:mt-6 {{ $loop->iteration === 1 ? 'mt-6' : '' }}">
                 @auth
                     <div x-data="{ show: false }" class="relative" @click.away="show = false">
                         <div @click="show = ! show" class="flex justify-center">
@@ -24,7 +24,7 @@
                             </button>
                         </div>
                         <div x-show="show" style="display:none"
-                            class="z-50 flex justify-around w-full px-4 py-2 mx-auto mt-2 mb-4 overflow-auto rounded-md bg-slate-900 text-slate-300 max-h-72">
+                            class="z-50 flex justify-around w-full px-4 py-2 mx-auto mt-2 mb-4 overflow-auto bg-black rounded-md text-slate-300 max-h-72">
                             @if (Auth::user())
                                 <x-list-add name="finished" :book="$book" />
                                 <x-list-add name="current" :book="$book" />
