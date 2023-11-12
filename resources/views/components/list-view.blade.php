917cc87046954cdd7162afd07963f6eb18c60660
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-12 08:51:18 -0500

    Update list-view.blade.php
    nav icon

--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -20,12 +20,10 @@
         {{-- Trigger --}}
         <div @click="show = ! show" class="flex justify-center">
             <button class="flex items-center py-2 mb-4 border-b-2">
-                More information
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 pt-2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
-                </svg>
+                <p class="pr-2">
+                    More information
+                </p>
+                <x-dropdown-icon />
             </button>
         </div>
 
