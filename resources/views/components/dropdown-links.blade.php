0ad2af814f30a786e6079c5118f1b36c93a739fb
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-13 19:59:07 -0500

    Update dropdown-links.blade.php
    unnecessary auth check causing issue with non verified users

--- resources/views/components/dropdown-links.blade.php
+++ resources/views/components/dropdown-links.blade.php
@@ -1,6 +1,4 @@
 <div x-show="show" style="display:none"
     class="z-50 flex justify-around w-full px-4 py-2 mx-auto mt-2 mb-4 overflow-auto rounded-md lg:absolute bg-slate-800 text-slate-300 max-h-72">
-    @if (Auth::user())
-        {{ $slot }}
-    @endif
+    {{ $slot }}
 </div>
