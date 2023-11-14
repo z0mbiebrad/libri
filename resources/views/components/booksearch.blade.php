e2cdd0f25b5f5128aa141ad6d0109eec7f35b585
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-13 21:36:17 -0500

    Update booksearch.blade.php
    rearanged and added conditional to keep ebook checked if that was the last one used

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -3,17 +3,18 @@
         <label for="bookSearch" class="block w-full py-2 text-center"></label>
 
         <div class="flex justify-center text-black rounded-lg">
-            <input id="bookSearch" name="bookSearch" class="rounded-lg"
-                placeholder="{{ ucwords(request('bookSearch')) ?? 'Name of book...' }}" required>
             <div class="flex items-center ml-2 space-x-2 text-slate-300">
+                <input type="checkbox" id="epub" value="epub" name="epub"
+                    {{ request('epub') === 'epub' ? 'checked' : '' }}>
+                <label for="epub">Ebook</label>
+                <input id="bookSearch" name="bookSearch" class="rounded-lg"
+                    placeholder="{{ ucwords(request('bookSearch')) ?? 'Name of book...' }}" required>
                 <input type="radio" id="intitle" value="intitle" name="searchBy"
                     {{ request('searchBy') === 'intitle' ? 'checked' : '' }} />
                 <label for="intitle">Title</label>
                 <input type="radio" id="inauthor" value="inauthor" name="searchBy"
                     {{ request('searchBy') === 'inauthor' ? 'checked' : '' }}>
                 <label for="inauthor">Author</label>
-                <input type="checkbox" id="epub" value="epub" name="epub">
-                <label for="epub">Ebook</label>
             </div>
         </div>
         <button class="block w-full py-2">Search</button>
