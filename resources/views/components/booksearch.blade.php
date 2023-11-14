7c09ec9d3c82829c3bfa7fd94d583d231d950b55
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-13 21:49:15 -0500

    Update booksearch.blade.php
    style update

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -1,14 +1,12 @@
-<div class="w-full mx-auto my-1 shadow-md bg-slate-900 text-slate-300 shadow-slate-600">
+<div class="mx-auto my-1 shadow-md bg-slate-900 text-slate-300 shadow-slate-600">
     <form action="{{ route('results.show') }}" class="text-lg font-semibold">
-        <label for="bookSearch" class="block w-full py-2 text-center"></label>
-
-        <div class="flex justify-center text-black rounded-lg">
-            <div class="flex items-center ml-2 space-x-2 text-slate-300">
+        <div class="pt-4 mx-auto text-center text-black rounded-lg">
+            <input id="bookSearch" name="bookSearch" class="w-1/2 rounded-lg lg:w-1/3"
+                placeholder="{{ ucwords(request('bookSearch')) ?? 'Name of book...' }}" required>
+            <div class="flex items-center justify-center pt-2 space-x-2 text-center text-slate-300">
                 <input type="checkbox" id="epub" value="epub" name="epub"
                     {{ request('epub') === 'epub' ? 'checked' : '' }}>
                 <label for="epub">Ebook</label>
-                <input id="bookSearch" name="bookSearch" class="rounded-lg"
-                    placeholder="{{ ucwords(request('bookSearch')) ?? 'Name of book...' }}" required>
                 <input type="radio" id="intitle" value="intitle" name="searchBy"
                     {{ request('searchBy') === 'intitle' ? 'checked' : '' }} />
                 <label for="intitle">Title</label>
