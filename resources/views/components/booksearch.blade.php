b031372ca04db685fdccee34e44c09106939e29f
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-14 19:53:46 -0500

    Update booksearch.blade.php
    for auth users only

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -3,17 +3,19 @@
         <div class="pt-4 mx-auto text-center text-black rounded-lg">
             <input id="bookSearch" name="bookSearch" class="w-1/2 rounded-lg lg:w-1/3"
                 placeholder="{{ ucwords(request('bookSearch')) ?: 'Name of book...' }}" required>
-            <div class="flex items-center justify-center pt-2 space-x-2 text-center text-slate-300">
-                <input type="checkbox" id="epub" value="epub" name="epub"
-                    {{ request('epub') === 'epub' ? 'checked' : '' }}>
-                <label for="epub">Ebook</label>
-                <input type="radio" id="intitle" value="intitle" name="searchBy"
-                    {{ request('searchBy') === 'intitle' ? 'checked' : '' }} />
-                <label for="intitle">Title</label>
-                <input type="radio" id="inauthor" value="inauthor" name="searchBy"
-                    {{ request('searchBy') === 'inauthor' ? 'checked' : '' }}>
-                <label for="inauthor">Author</label>
-            </div>
+            @auth
+                <div class="flex items-center justify-center pt-2 space-x-2 text-center text-slate-300">
+                    <input type="checkbox" id="epub" value="epub" name="epub"
+                        {{ request('epub') === 'epub' ? 'checked' : '' }}>
+                    <label for="epub">Ebook</label>
+                    <input type="radio" id="intitle" value="intitle" name="searchBy"
+                        {{ request('searchBy') === 'intitle' ? 'checked' : '' }} />
+                    <label for="intitle">Title</label>
+                    <input type="radio" id="inauthor" value="inauthor" name="searchBy"
+                        {{ request('searchBy') === 'inauthor' ? 'checked' : '' }}>
+                    <label for="inauthor">Author</label>
+                </div>
+            @endauth
         </div>
         <button class="block w-full py-2">Search</button>
     </form>
