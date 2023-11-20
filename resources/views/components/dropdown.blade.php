8e445254d74f26fcabcfff112c77e12a402968d5
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2023-11-20 15:25:14 -0500

    Dropdown finale

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,14 +1,14 @@
-<div x-data="{ show: false }" class="" @click.away="show = false">
-    <div @click="show = ! show" class="">
-        <div class="flex items-center py-2 mb-2 border-b-2">
+<div x-data="{ open: false }" class="mx-0" @click.away="open = false">
+    <div x-on:click="open = ! open">
+        <div class="flex items-center p-2 mb-2 shadow-sm shadow-gray-300">
             <p class="pr-2">
                 {{ $trigger }}
             </p>
             <x-dropdown-icon />
         </div>
     </div>
-    <div x-show="show" style="display:none"
-        class="z-50 w-full px-4 py-2 mx-auto mt-2 mb-4 overflow-auto rounded-md bg-slate-800 max-h-72">
+    <div x-show="open" style="display:none" x-transition
+        class="z-50 w-full px-4 py-2 mt-2 mb-4 overflow-auto rounded-md -mx-auto bg-slate-800 max-h-72">
         {{ $content }}
     </div>
 </div>
--- resources/views/components/list-view.blade.php
+++ resources/views/components/list-view.blade.php
@@ -1,67 +1,41 @@
-@if (Request::is('list/*'))
-    <x-dropdown>
-        <x-slot name="trigger">
-            Book Options
-        </x-slot>
-        <x-slot name="content">
-            <div class="flex justify-center">
-                <x-book-delete :book="$book" />
-                <x-list-update :book="$book" />
-            </div>
-        </x-slot>
-    </x-dropdown>
-@else
-    <div class="flex justify-between">
-        <x-dropdown>
-            <x-slot name="trigger">
-                Add to List
-            </x-slot>
-            <x-slot name="content">
-                <div class="flex justify-center">
-                    <x-list-add name="finished" :book="$book" />
-                    <x-list-add name="current" :book="$book" />
-                    <x-list-add name="wishlist" :book="$book" />
-                </div>
-            </x-slot>
-        </x-dropdown>
-        <x-dropdown>
-            <x-slot name="trigger">
-                More information
-            </x-slot>
+{{-- card about --}}
+<x-dropdown>
+    <x-slot name="trigger">
+        More information
+    </x-slot>
 
-            <x-slot name="content">
+    <x-slot name="content">
 
-                <div class="space-y-2 italic">
-                    @isset($book->subtitle)
-                        <p>
-                            {{ $book->subtitle }}
-                        </p>
-                    @endisset
-                    @isset($book->categories)
-                        <p>
-                            {{ $book->categories }}
-                        </p>
-                    @endisset
-                    @isset($book->published_date)
-                        <p>
-                            {{ $book->published_date }}
-                        </p>
-                    @endisset
-                    @isset($book->rating)
-                        <p class="flex items-center">
-                            {{ $book->rating }}
-                            <x-rating-icon />
-                        </p>
-                    @endisset
-                    <p class="pb-3 not-italic">
-                        {!! strip_tags($book->description, '<b><br><i>') ?? 'Description not available.' !!}
-                    </p>
-                </div>
-            </x-slot>
-        </x-dropdown>
-    </div>
-@endif
+        <div class="space-y-2 italic">
+            @isset($book->subtitle)
+                <p>
+                    {{ $book->subtitle }}
+                </p>
+            @endisset
+            @isset($book->categories)
+                <p>
+                    {{ $book->categories }}
+                </p>
+            @endisset
+            @isset($book->published_date)
+                <p>
+                    {{ $book->published_date }}
+                </p>
+            @endisset
+            @isset($book->rating)
+                <p class="flex items-center">
+                    {{ $book->rating }}
+                    <x-rating-icon />
+                </p>
+            @endisset
+            <p class="pb-3 not-italic">
+                {!! strip_tags($book->description, '<b><br><i>') ?? 'Description not available.' !!}
+            </p>
+        </div>
+    </x-slot>
+</x-dropdown>
 
+{{-- card body --}}
 <div class="px-3">
     <div class="mx-auto text-center">
 
@@ -85,5 +59,32 @@
             <a href="{{ $book->epub }}">eBook {{ $book->price }}</a>
         </div>
     @endisset
-
 </div>
+
+{{-- Card header --}}
+@if (Request::is('list/*'))
+    <x-dropdown>
+        <x-slot name="trigger">
+            Book Options
+        </x-slot>
+        <x-slot name="content">
+            <div class="flex justify-center">
+                <x-book-delete :book="$book" />
+                <x-list-update :book="$book" />
+            </div>
+        </x-slot>
+    </x-dropdown>
+@else
+    <x-card-dropdown>
+        <x-slot name="trigger">
+            Add to List
+        </x-slot>
+        <x-slot name="content">
+            <div class="flex justify-center">
+                <x-list-add name="finished" :book="$book" />
+                <x-list-add name="current" :book="$book" />
+                <x-list-add name="wishlist" :book="$book" />
+            </div>
+        </x-slot>
+    </x-card-dropdown>
+@endif
