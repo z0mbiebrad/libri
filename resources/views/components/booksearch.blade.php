79be3b590364d3dba54d2e15c5f37bcc4ffbdce9
z0mbiebrad <bradleyt.evans@gmail.com>
2025-01-07 13:52:38 -0500

    Changed nav and search bar to look better

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -1,8 +1,14 @@
-<div class="mx-auto border-b shadow-inner border-slate-600 bg-slate-900 shadow-slate-600">
+<div class="mx-auto border-slate-600 mt-4" x-data="{ bookSearch: '' }">
     <form action="{{ route('results.show') }}" class="text-lg font-semibold">
         <div class="pt-4 mx-auto text-center text-black rounded-lg">
-            <input id="bookSearch" name="bookSearch" class="w-1/2 rounded-lg lg:w-1/3"
-                placeholder="{{ ucwords(request('bookSearch')) ?: 'Name of book...' }}" required>
+            <input
+                id="bookSearch" 
+                name="bookSearch" 
+                class="w-1/2 rounded-lg lg:w-1/3"
+                placeholder="{{ ucwords(request('bookSearch')) ?: 'Name of book...' }}" 
+                required
+                x-model="bookSearch"
+            >
             @auth
                 <div class="flex items-center justify-center pt-2 space-x-2 text-center text-white">
                     <input type="radio" id="intitle" value="intitle" name="searchBy"
@@ -14,9 +20,16 @@
                 </div>
             @endauth
         </div>
-        <div class="flex justify-center">
+        <div 
+            class="flex justify-center"
+            x-show="bookSearch.length > 0"
+            x-transition
+        >
             <button
-                class="w-1/3 py-2 my-2 text-center rounded-lg shadow-inner shadow-gray-300 hover:bg-slate-800 hover:shadow-white sm:w-1/4 md:w-1/5 lg:w-1/6">Search</button>
+                class="mt-6 w-1/3 py-2 my-2 text-center rounded-lg shadow-inner bg-slate-900 shadow-gray-300 hover:bg-slate-800 hover:shadow-white sm:w-1/4 md:w-1/5 lg:w-1/6"
+            >
+                Search
+            </button>
         </div>
     </form>
 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,143 +1,74 @@
-<nav x-data="{ show: false }" class="border-b shadow-md border-slate-600 shadow-slate-600 bg-slate-900">
-    <!-- Primary Navigation Menu -->
-    <div class="px-4 mx-auto sm:px-2 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="flex items-center shrink-0">
-                    <a href="{{ route('booksearch') }}" class="mr-2 text-xl">
-                        <x-application-logo class="block w-auto text-gray-800 fill-current h-9" />
-                    </a>
-                </div>
-
-                @if (Auth::user())
-                    <!-- Navigation Links -->
-                    <div class="hidden 2xl:space-x-10 xl:space-x-8 lg:space-x-6 md:space-x-4 sm:space-x-8 sm:flex">
-                        <x-nav-link class="sm:ml-4" :href="route('booksearch')" :active="request()->routeIs(['booksearch', 'results.show'])">
+<nav class="border-gray-200 py-6">
+    <div class="container mx-auto flex flex-wrap items-center justify-between">
+        <a href="#" class="flex">
+            <x-application-logo class="block w-auto text-gray-800 fill-current h-9" />
+            <span class="self-center text-lg font-semibold whitespace-nowrap">Librisearch</span>
+        </a>
+        <button data-collapse-toggle="mobile-menu" type="button" class="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+            </svg>
+            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+        </button>
+        <div class="hidden md:block w-full md:w-auto" id="mobile-menu">
+            <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+                @if (Auth::check())
+                    <li>
+                        <a href="{{ route('booksearch') }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->routeIs('booksearch') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
                             {{ __('Search') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('list.index', ['list' => 'finished'])" :active="Request::is('list/finished')">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('list.index', ['list' => 'finished']) }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->is('list/finished') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
                             {{ __('Finished') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('list.index', ['list' => 'current'])" :active="Request::is('list/current')">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('list.index', ['list' => 'current']) }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->is('list/current') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
                             {{ __('Current') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('list.index', ['list' => 'wishlist'])" :active="Request::is('list/wishlist')">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('list.index', ['list' => 'wishlist']) }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->is('list/wishlist') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
                             {{ __('Wishlist') }}
-                        </x-nav-link>
-                    </div>
-                @else
-                    <div class="hidden sm:space-x-8 sm:-my-px sm:flex sm:ml-4">
-                        <x-nav-link :href="route('booksearch')" :active="request()->routeIs('booksearch')">
-                            {{ __('Search') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                            {{ __('Login') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                            {{ __('Create Account') }}
-                        </x-nav-link>
-                    </div>
-                @endif
-            </div>
-            @if (Auth::user())
-                <!-- Settings Dropdown -->
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="">
-                                <div>{{ Auth::user()->name }}</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('profile.edit') }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->routeIs('profile.edit') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
+                            {{ __('Profile') }}
+                        </a>
+                    </li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="block pl-3 pr-4 py-2 rounded md:p-0 text-gray-700 hover:text-blue-700">
+                                {{ __('Log Out') }}
                             </button>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
-            @endif
-
-
-
-            <!-- Hamburger -->
-            <div class="flex items-center -mr-2 sm:hidden">
-                <button @click="show = ! show"
-                    class="inline-flex items-center justify-center p-2 text-white transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-slate-600 focus:outline-none focus:bg-slate-700 focus:text-white">
-                    @auth
-                        <p class="mr-2">{{ Auth::user()->name }}</p>
-                    @else
-                        <p class="mr-2">Menu</p>
-                    @endauth
-                    <x-dropdown-icon />
-                </button>
-            </div>
+                        </form>
+                    </li>
+                @else
+                    <li>
+                        <a href="{{ route('booksearch') }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->routeIs('booksearch') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
+                            {{ __('Search') }}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('login') }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->routeIs('login') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
+                            {{ __('Login') }}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('register') }}" class="block pl-3 pr-4 py-2 rounded md:p-0 {{ request()->routeIs('register') ? 'text-blue-700 font-semibold' : 'text-gray-700 hover:text-blue-700' }}">
+                            {{ __('Create Account') }}
+                        </a>
+                    </li>
+                @endif
+            </ul>
         </div>
     </div>
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': show, 'hidden': !show }" class="hidden sm:hidden">
-        @if (Auth::user())
-            <div class="pt-2 pb-3 space-y-1 shadow-inner shadow-slate-600">
-                <x-responsive-nav-link :href="route('booksearch')" :active="request()->routeIs(['booksearch', 'results.show'])">
-                    {{ __('Search') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('list.index', ['list' => 'finished'])" :active="Request::is('list/finished')">
-                    {{ __('Finished') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('list.index', ['list' => 'current'])" :active="Request::is('list/current')">
-                    {{ __('Current') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('list.index', ['list' => 'wishlist'])" :active="Request::is('list/wishlist')">
-                    {{ __('Wishlist') }}
-                </x-responsive-nav-link>
-            </div>
-            <!-- Responsive Settings Options -->
-            <div class="pt-4 pb-1 border-t shadow-inner border-slate-600 shadow-slate-600">
-                <div class="px-4">
-                    <div class="text-base font-medium">{{ Auth::user()->name }}</div>
-                    <div class="text-sm font-medium">{{ Auth::user()->email }}</div>
-                </div>
-
-                <div class="mt-3 space-y-1">
-                    <x-responsive-nav-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
-                    </x-responsive-nav-link>
-
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                            this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-responsive-nav-link>
-                    </form>
-                </div>
-            </div>
-        @else
-            <x-responsive-nav-link :href="route('booksearch')" :active="request()->routeIs('booksearch')">
-                {{ __('Book Search') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                {{ __('Login') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                {{ __('Create Account') }}
-            </x-responsive-nav-link>
-        @endif
-    </div>
 </nav>
+
+<script src="https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js"></script>
