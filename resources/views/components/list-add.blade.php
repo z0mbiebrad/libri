ebc1a2b56032ba860e0ead80799a33bd9d6a9fc7
z0mbiebrad <bradleyt.evans@gmail.com>
2025-03-08 00:20:26 -0500

    wip

--- resources/views/components/list-add.blade.php
+++ resources/views/components/list-add.blade.php
@@ -30,7 +30,7 @@
 
     @foreach ($allLists as $list)
         <form action="{{ route('book.store', [$bookSearch, $list, $book]) }}" method="POST"
-            class="bg-neutral-50 px-4 py-1 text-sm text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
+            class="bg-neutral-50 px-4 py-1 text-base text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
             @csrf
             <input type="hidden" value="{{ $book }}" class="flex items-center">
             <button type="submit" class="hover:cursor-pointer mx-auto italic">
