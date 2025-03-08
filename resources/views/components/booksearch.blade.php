8555ed95b2429116303061d50fe4cb840b8ce652
z0mbiebrad <bradleyt.evans@gmail.com>
2025-03-07 23:58:31 -0500

    wip

--- resources/views/components/booksearch.blade.php
+++ resources/views/components/booksearch.blade.php
@@ -9,6 +9,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
-        </button>
+            <input type="search" class="w-full rounded-sm border border-neutral-300 bg-neutral-50 py-2 pl-10 pr-2 text-[16px] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white" name="bookSearch" placeholder="Search for a book" aria-label="search"/>
+        </div>
     </form>
 </div>
