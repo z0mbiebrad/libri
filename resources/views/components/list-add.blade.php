84eafa6a4df271f2d6d204271cf523f13eb9db31
z0mbiebrad <bradleyt.evans@gmail.com>
2025-03-08 00:19:14 -0500

    wip

--- resources/views/components/list-add.blade.php
+++ resources/views/components/list-add.blade.php
@@ -7,7 +7,7 @@
 
     @foreach ($availableLists as $list)
         <form action="{{ route('book.update', [$book, $list]) }}" method="post"
-            class="bg-neutral-50 px-4 py-1 text-sm text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
+            class="bg-neutral-50 px-4 py-1 text-base text-neutral-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-neutral-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
             @csrf
             <button type="submit" class="hover:cursor-pointer mx-auto italic">
                 {{ ucwords($list) }}
@@ -15,7 +15,7 @@
         </form>
     @endforeach
     <form action="{{ route('book.destroy', $book) }}" method="post"
-        class="bg-neutral -50 px-4 py-1 text-sm text-red-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-red-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
+        class="bg-neutral -50 px-4 py-1 text-base text-red-600 hover:bg-neutral-900/5 hover:text-neutral-900 focus-visible:bg-neutral-900/10 focus-visible:text-neutral-900 focus-visible:outline-hidden dark:bg-neutral-900 dark:text-red-300 dark:hover:bg-neutral-50/5 dark:hover:text-white dark:focus-visible:bg-neutral-50/10 dark:focus-visible:text-white hover:cursor-pointer">
         @csrf
         @method('DELETE')
         <button type="submit" class="hover:cursor-pointer mx-auto italic">
